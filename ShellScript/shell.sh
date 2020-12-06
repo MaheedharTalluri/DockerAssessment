72a2a295b294bf92e8426dc7fa2320cfd24c8fed
Talluri <maheedhar.talluri@bt.com>
2020-12-06 11:42:56 +0530

    shellscript added

--- ShellScript/shell.sh
+++ ShellScript/shell.sh
@@ -1,9 +1,5 @@
 mkdir docker
 cp Dockerfile.txt ./docker/Dockerfile.txt
 cd docker
-git init
-git commit -am "Dockerfile added"
-git remote add origin $1
-git push origin master
 docker build -f Dockerfile -t test:0.1 .
 docker push test:0.1
